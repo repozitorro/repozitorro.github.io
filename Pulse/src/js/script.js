a6b00fea8b37b5e326792cfe3a4fb11d738dc2f9
Bogdan <repozitorro@gmail.com>
2020-02-23 14:41:03 +0200

    Added form validation

--- Pulse/src/js/script.js
+++ Pulse/src/js/script.js
@@ -54,4 +54,31 @@ $(document).ready(function () {
         });
     });
 
+    //validation form
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+380 (99) 999-99-99");
 });
